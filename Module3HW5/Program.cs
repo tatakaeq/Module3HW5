71f7dada45bb0033534914289ec2bd737b85fc06
Andrew Kara <a.kara701@gmail.com>
2021-12-18 00:47:35 +0200

    Probably done

---
+++ Module3HW5/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Module3HW5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var result = GetResultAsync().GetAwaiter().GetResult();
+            Console.WriteLine(result);
+        }
+
+        public static async Task<string> ReadHelloAsync()
+        {
+            using (var reader = new StreamReader("Hello.txt"))
+            {
+                var hello = await reader.ReadToEndAsync();
+                return hello;
+            }
+        }
+
+        public static async Task<string> ReadWorldAsync()
+        {
+            using var reader = new StreamReader("World.txt");
+            var world = await reader.ReadToEndAsync();
+            return world;
+        }
+
+        public static async Task<string> GetResultAsync()
+        {
+            Console.WriteLine("Get REsult begin");
+            var hello = await Task.Run(() => ReadHelloAsync());
+            var world = await Task.Run(() => ReadWorldAsync());
+            return hello + world;
+        }
+    }
+}
