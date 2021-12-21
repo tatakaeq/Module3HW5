f361b82a187f15327cc4f6873313ea159931a347
Andrew Kara <a.kara701@gmail.com>
2021-12-21 19:18:44 +0200

    Final fix x3

--- Module3HW5/Program.cs
+++ Module3HW5/Program.cs
@@ -11,25 +11,22 @@
         public static async Task<string> ReadHelloAsync()
         {
             using var reader = new StreamReader("Hello.txt");
-            Thread.Sleep(5000);
             return await reader.ReadToEndAsync();
         }
 
         public static async Task<string> ReadWorldAsync()
         {
             using var reader = new StreamReader("World.txt");
-            Thread.Sleep(2000);
             return await reader.ReadToEndAsync();
         }
 
         public static async Task<string> GetResultAsync()
         {
-            Console.WriteLine("GetResult begin");
             var hello = Task.Run(async () => await ReadHelloAsync());
             var world = Task.Run(async () => await ReadWorldAsync());
 
-            await Task.WhenAll(new[] { hello, world });
-            return hello.GetAwaiter().GetResult() + world.GetAwaiter().GetResult();
+            var result = await Task.WhenAll(new[] { hello, world });
+            return string.Join(" ", result);
         }
     }
 }
